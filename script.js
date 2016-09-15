9e436cd1ce07949974d0806f7e00a7da8940a509
Lucy Zhang <spothorse9.lucy@gmail.com>
2016-09-14 20:48:59 -0400

    Did some styling

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 var fallSemesterEnd_2016 = new Date(2016,12,19);
+var fallBreakStart = new Date(2016,10,07);
+var fallBreakEnd = new Date(2016,10,12);
+var thanksgivingBreakStart=new Date(2016,11,22);
+var thanksgivingBreakEnd=new Date(2016,11,28);
 
 function calculateFoodPoints() {
     console.log("clicked");
@@ -9,6 +13,9 @@ function calculateFoodPoints() {
     }
     var today = new Date();
     var numDays = getDateDiff(today, fallSemesterEnd_2016);
+    var fallBreak = getDateDiff(fallBreakStart,fallBreakEnd);
+    var thanksgivingBreak = getDateDiff(thanksgivingBreakStart,thanksgivingBreakEnd);
+    numDays=numDays-fallBreak-thanksgivingBreak;
     var pointsPerDay = parseInt(numFoodPts/numDays);
     updateUI(pointsPerDay);
     console.log("done");
@@ -22,7 +29,6 @@ function getDateDiff(firstDate, secondDate) {
 }
 
 function updateUI(pointsPerDay){
-	var pointsString="You should spend "+pointsPerDay.toString()+" food points per day until the end of the fall semester." 
-	alert(pointsString);
+	var pointsString="You should spend <h2>"+pointsPerDay.toString()+"</h2> food points per day until the end of the fall semester." 
 	$("#results").html(pointsString);
 }
